5bc5f9ded36750c64cc75184814e2b63b0b15959
Joshua Klein <mobiusklein@gmail.com>
2025-05-30 17:39:33 -0400

    chore: version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     name="pyteomics.cythonize",
     description="An Cython-accelerated version of common pyteomics functions",
     long_description=open("README.rst").read(),
-    version="0.2.7",
+    version="0.2.8",
     packages=['pyteomics'],
     zip_safe=False,
     package_dir={"pyteomics": '.'},
