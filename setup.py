bbbc8aae7fba4b82f8c4e2452f179107e9b6b91a
Joshua Klein <mobiusklein@gmail.com>
2025-06-06 22:56:12 -0400

    update packaging script

--- setup.py
+++ setup.py
@@ -3,19 +3,17 @@ from setuptools import setup, Extension, find_namespace_packages
 
 try:
     from Cython.Build import cythonize
-    extensions = cythonize([
-        Extension(name="pyteomics.cparser", sources=[
-                  "pyteomics/cparser.pyx"]),
-        Extension(name="pyteomics.cmass",
-                  sources=["pyteomics/cmass.pyx"])
-    ])
+    extensions = cythonize(
+        [
+            Extension(name="pyteomics.cparser", sources=["pyteomics/cparser.pyx"]),
+            Extension(name="pyteomics.cmass", sources=["pyteomics/cmass.pyx"]),
+        ]
+    )
 except ImportError:
-    extensions = ([
-        Extension(name="pyteomics.cparser",
-                  sources=["pyteomics/cparser.c"]),
-        Extension(name="pyteomics.cmass",
-                  sources=["pyteomics/cmass.c"])
-    ])
+    extensions = [
+        Extension(name="pyteomics.cparser", sources=["pyteomics/cparser.c"]),
+        Extension(name="pyteomics.cmass", sources=["pyteomics/cmass.c"]),
+    ]
 
 
 setup(
@@ -23,9 +21,8 @@ setup(
     description="An Cython-accelerated version of common pyteomics functions",
     long_description=open("README.rst").read(),
     version="0.2.8",
-    packages=['pyteomics'],
+    packages=find_namespace_packages(include=["pyteomics*"]),
     zip_safe=False,
-    package_dir={"pyteomics": '.'},
     install_requires=["pyteomics"],
     include_package_data=True,
     ext_modules=extensions,
@@ -33,7 +30,6 @@ setup(
     maintainer_email="jaklein@bu.edu",
     classifiers=[
         "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Topic :: Education",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
